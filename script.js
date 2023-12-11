6ec050e9caa45d2e3f49570eef5a6213ed77524b
Figurante137 <vvitorhenrique5@gmail.com>
2023-12-11 14:39:00 -0300

    git add . git commit -m "por enquanto"

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 const pesquisa = document.getElementById("barra_pesquisa");
+const conteudo = document.getElementById("conteudo-inicial");
+const fot = document.getElementById("o-footer");
+const home = document.getElementById("logo");
+const grupocart = document.getElementById("o-cart")
+const cart = document.getElementById("icon_cart");
+const area_pesquisa = document.getElementById("area_de_pesquisa");
 pesquisa.addEventListener("input", function(){
     let valor_pesquisa = pesquisa.value
     console.log(valor_pesquisa)
+})
+cart.addEventListener("click", function(){
+    conteudo.classList.add("desliga")
+    fot.classList.add("desliga")
+    area_pesquisa.classList.add("desliga")
+    grupocart.classList.remove("desliga")
+})
+home.addEventListener("click", function(){
+    conteudo.classList.remove("desliga")
+    fot.classList.remove("desliga")
+    area_pesquisa.classList.add("desliga")
+    grupocart.classList.add("desliga")
 })
---
+++ script_cart.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const addToCartButtons = document.querySelectorAll('.add-to-cart');
+    const cartItemsContainer = document.getElementById('cart-items');
+    const cartTotal = document.getElementById('cart-total');
+    const checkoutButton = document.querySelector('.checkout-button');
+    const cart = [];
+  
+    addToCartButtons.forEach(button => {
+      button.addEventListener('click', addToCart);
+    });
+  
+    function addToCart(e) {
+      const button = e.target;
+      const product = button.closest('.card'); // Encontra o elemento pai mais próximo com a classe 'card'
+      
+      // Verifica se o produto foi encontrado
+      if (product) {
+        const productName = product.dataset.name;
+        const productPrice = parseFloat(product.dataset.price);
+  
+        const item = { name: productName, price: productPrice };
+        cart.push(item);
+  
+        updateCart();
+      }
+    }
+  
+    function updateCart() {
+      cartItemsContainer.innerHTML = '';
+      let total = 0;
+  
+      cart.forEach(item => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${item.name} - R$ ${item.price.toFixed(2)}`;
+        cartItemsContainer.appendChild(listItem);
+  
+        total += item.price;
+      });
+  
+      cartTotal.textContent = `R$ ${total.toFixed(2)}`;
+    }
+  
+    checkoutButton.addEventListener('click', function() {
+      checkout();
+    });
+  
+    function checkout() {
+      if(cart.length > 0){
+      alert('Compra realizada com sucesso!');
+      cart.length = 0; // Limpar o carrinho
+      updateCart(); // Atualizar o carrinho após a compra
+      }
+      else{
+        alert('Nenhum item no carrinho');
+      }
+    }
+  });
